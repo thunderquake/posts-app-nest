5c220058ddd608be876db13d37daecf53884cd76
Anna Dmytriieva <loooverrr12@gmail.com>
2024-11-18 18:49:02 +0200

    fix: token verification

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -34,9 +34,7 @@ export class AuthGuard implements CanActivate {
       throw new UnauthorizedException();
     }
     try {
-      const payload = await this.jwtService.verifyAsync(token, {
-        secret: `${process.env.JWT_SECRET}`,
-      });
+      const payload = await this.jwtService.verifyAsync(token);
       request['user'] = payload;
     } catch (e) {
       this.logger.error('JWT verification failed', e);
