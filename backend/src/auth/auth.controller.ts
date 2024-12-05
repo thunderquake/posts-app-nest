70562990f33363a483dad15a971ee1b7cdbb2fe6
Anna Dmytriieva <loooverrr12@gmail.com>
2024-12-05 16:46:57 +0200

    feat: add send new email route

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -6,10 +6,12 @@ import {
   HttpStatus,
   Post,
   Request,
+  UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
 import { Public } from 'src/decorators/public.decorator';
 import { EmailConfirmationService } from 'src/mail-confirmation/mail-confirmation.service';
+import { UserService } from 'src/user/user.service';
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
@@ -18,6 +20,7 @@ import { SignUpDto } from './dto/sign-up.dto';
 @Controller('auth')
 export class AuthController {
   constructor(
+    private userService: UserService,
     private authService: AuthService,
     private readonly emailConfirmationService: EmailConfirmationService,
   ) {}
@@ -48,5 +51,22 @@ export class AuthController {
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Public()
+  @Post('send-verification-email')
+  async resendVerificationEmail(@Body('email') email: string) {
+    const user = await this.userService.findByEmail(email);
+    if (!user || user.isVerified) {
+      throw new UnauthorizedException('User not found or already verified');
+    }
+
+    await this.emailConfirmationService.sendVerificationLink(email);
+
+    return {
+      message:
+        'A new verification email has been sent. Please check your inbox.',
+    };
   }
 }
